fcda5d3aa2fa666e7f2bf16d59b706c57694931e
Prateek20042 <221B274@JUETGUNA.IN>
2025-03-28 14:18:15 +0530

    Add files via upload

---
+++ le5/Beer.java
@@ -0,0 +1,8 @@
+class Beer extends Beverages{
+    protected void addCont(){
+        System.out.println("add nothing ");
+    }
+    protected void serve(){
+    System.out.println("served by the waiter");
+    }
+}
---
+++ le5/Rum.java
@@ -0,0 +1,11 @@
+class Rum extends Beverages{
+    protected void addCont(){
+        System.out.println("add ice ");
+    }
+    protected void stir(){
+        System.out.println("stir for 20 sec");
+    }
+    protected void serve(){
+    System.out.println("served by the waiter");
+    }
+}
